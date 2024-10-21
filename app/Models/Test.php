1b9bdf2adf6879e6fac9c9cae9ae781ccc03180c
Elisabet (Jitera) <developers@jitera.com>
2024-10-21 15:24:33 +0000

    feat[database]: create table 'tests' Changes * '/app/Models/Test.php': created * '/database/migrations/2023_04_21_142424_create_tests_table.php': created

---
+++ app/Models/Test.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Test extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        // Assuming columns to be filled later
+    ];
+
+    protected $casts = [
+        // Assuming casts to be defined later
+    ];
+
+    // Assuming relationships to be defined later
+}
